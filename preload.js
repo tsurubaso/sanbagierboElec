bc534d3cef6bb52bbbd3be9c696b9232d5b029d0
tsurubaso <tsurubaso@hotmail.com>
2025-11-20 20:08:01 +0900

    ok rien ne marche tout plante

--- preload.js
+++ preload.js
@@ -26,7 +26,6 @@ contextBridge.exposeInMainWorld("electronAPI", {
   githubSync: () => ipcRenderer.invoke("github-sync"),
   onAuthSuccess: (callback) => {
     ipcRenderer.on("auth-success", callback);
-    return () => ipcRenderer.removeListener("auth-success", callback);
-  
-},
+    return () => ipcRenderer.removeListener("auth-success", callback);},
+  createOrUpdateBook: (fileName, content) =>ipcRenderer.invoke("create-or-update-book", { fileName, content }),
 });
